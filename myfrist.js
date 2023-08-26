f8ccee172c6a11984af2344f1f9085f2f92cbd79
Dorik setyawan <dorik.setyawan@zettacamp.pro>
2023-08-26 22:00:52 +0700

    JS_Day1

---
+++ myfrist.js
@@ -0,0 +1,34 @@
+let bookFav1 = 'Harry porter';
+bookFav1="Sumanto "; //update value from bookFav  TASK 1a =============
+
+const bookFav3 = "Hunter on the darkness"; 
+// bookFav3='Nothing to lose'; //update value from bookFav const  TASK 1b =================================
+console.log(bookFav1);   // task 1=====
+console.log(bookFav3); // task 1=====
+
+// let favoriteBook = bookFav1.concat(' ',bookFav3/*,' ',bookFav2*/);  // TASK 2 ============
+// console.log(favoriteBook);
+
+// let bookList = ["Drawing Book", "Notebook"]; // Try array
+// console.log(bookList);
+
+// const person = {  // try const with Object
+//     name: 'Dorik',
+//     age: 24,
+// }
+// person.address = 'Bantul'
+// console.log(person);
+// ====================================================================================================================================
+// TASK JS DAY 2
+//comparasion
+console.log("Comparasion Books Fav");
+const comparasionBookFav = bookFav1 != bookFav3 ? "True" : "False"  // apakah bukuFav1 sama dengan bukuFav3? Iya ? menampilkan pesan Iya. jika bukan menampilkan tidak.
+console.log(comparasionBookFav);
+// console.log(bookFav1 == bookFav3 );  // equal return true if name of fav books same.
+// console.log(bookFav1 != bookFav3);  // not equal return true if name of fav book defference
+// console.log(bookFav1 === bookFav3 );  // strict equal return true if name character is same .
+// console.log(bookFav1 !== bookFav3 );  // strict not equal return true if name of fav book defference
+
+
+
+
