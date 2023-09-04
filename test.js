dda6fa7382d2346ed45b1c31fdea88255874c1d9
Dorik setyawan <dorik.setyawan@zettacamp.pro>
2023-09-04 17:02:47 +0700

    Debugging

---
+++ test.js
@@ -0,0 +1,28 @@
+function innerFunction() {
+    debugger; // Ini adalah breakpoint di dalam fungsi innerFunction()
+    // Kode lain di dalam innerFunction()
+}
+function innerFunction() {
+    debugger; // Ini adalah breakpoint di dalam fungsi innerFunction()
+    // Kode lain di dalam innerFunction()
+}
+function innerFunction() {
+    debugger; // Ini adalah breakpoint di dalam fungsi innerFunction()
+    // Kode lain di dalam innerFunction()
+}
+function innerFunction() {
+    debugger; // Ini adalah breakpoint di dalam fungsi innerFunction()
+    // Kode lain di dalam innerFunction()
+}
+function innerFunction() {
+    debugger; // Ini adalah breakpoint di dalam fungsi innerFunction()
+    // Kode lain di dalam innerFunction()
+}
+function innerFunction() {
+    debugger; // Ini adalah breakpoint di dalam fungsi innerFunction()
+    // Kode lain di dalam innerFunction()
+}
+function innerFunction() {
+    debugger; // Ini adalah breakpoint di dalam fungsi innerFunction()
+    // Kode lain di dalam innerFunction()
+}
